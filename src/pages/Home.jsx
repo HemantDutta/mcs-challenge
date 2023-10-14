cc3515257e0f67eceec9e01e1ad7e387b3cf18fd
HemantDutta <hemantnorddutta2@gmail.com>
2023-10-14 16:05:45 +0530

    Create About Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -152,8 +152,45 @@ export const Home = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="join-us-notice">
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
+                        <span>* For forms AOC-4 and MGT-7, you will be charged a penalty of ₹200 <span className="bold">every day</span> until you file the form . There is no maximum penalty amount. So, if you don't file the form for a year, you will owe ₹73,000 per form</span>
+                    </div>
                 </div>
                 {/*  Join Us End  */}
+                {/*  About  */}
+                <div className="about" id="about">
+                    <div className="about-left">
+                        <div className="left-header">
+                            <span><span className="highlight">All-in-One</span> platform that Makes Easier</span>
+                        </div>
+                        <div className="left-tag">
+                            <span>We are more than a platform; We are your success partner. Discover our services to achieve your business and educational goals</span>
+                        </div>
+                        <div className="left-grid">
+                            <div className="grid-item">
+                                <img src="src/assets/search_icon.png" alt=""/>
+                                <span><span className="bold">SEARCH</span> for vital company information</span>
+                            </div>
+                            <div className="grid-item">
+                                <img src="src/assets/connect_icon.png" alt=""/>
+                                <span><span className="bold">CONNECT</span> with the resources to meet your business needs</span>
+                            </div>
+                            <div className="grid-item">
+                                <img src="src/assets/research_icon.png" alt=""/>
+                                <span><span className="bold">RESEARCH</span> and generate report that drive growth </span>
+                            </div>
+                            <div className="grid-item">
+                                <img src="src/assets/search_icon.png" alt=""/>
+                                <span><span className="bold">ACADEMY</span> to give you the skills for success in your career</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="about-right">
+                        <img src="src/assets/about_img.png" alt="About"/>
+                    </div>
+                </div>
+                {/*  About End  */}
             </div>
         </>
     )
