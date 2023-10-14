30d598ac3596ffd7a1d7580f6d5f8e08e14c8edf
HemantDutta <hemantnorddutta2@gmail.com>
2023-10-14 16:59:38 +0530

    Create Big Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,24 @@
 import {Link} from "react-router-dom";
 
 export const Navbar = () => {
+
+    function toggleBigMenu(){
+        let big = document.getElementById("bigMenu");
+
+        if(big.classList.contains("active")){
+            big.classList.remove("active");
+            setTimeout(()=>{
+                big.style.display = "none";
+            },400)
+        }
+        else{
+            big.style.display = "grid";
+            setTimeout(()=>{
+                big.classList.add("active");
+            },50);
+        }
+    }
+
     return(
         <>
             <nav>
@@ -16,9 +34,22 @@ export const Navbar = () => {
                 <div className="navbar-buttons">
                     <button type="button" className="login-btn">Login</button>
                     <button type="button" className="reg-btn">Register</button>
-                    <button type="button" className="ham-btn"><i className="fa-solid fa-bars"/></button>
+                    <button type="button" className="ham-btn" onClick={toggleBigMenu}><i className="fa-solid fa-bars"/></button>
                 </div>
             </nav>
+            <div className="bigMenu" id="bigMenu">
+                <div className="close-btn" onClick={toggleBigMenu}>
+                    <i className="fa-solid fa-x"/>
+                </div>
+                <ul>
+                    <li><Link to={"/"}>Solutions</Link></li>
+                    <li><Link to={"/"}>Features</Link></li>
+                    <li><Link to={"/"}>Blogs</Link></li>
+                    <li><Link to={"/"}>About</Link></li>
+                    <li><button type="button" className="login-btn">Login</button></li>
+                    <li><button type="button" className="reg-btn">Register</button></li>
+                </ul>
+            </div>
         </>
     )
 }
